0e4dafc4feb9c9acb57cd277eaf299e06baf7968
unknown <iamonfire@yandex.ru>
2022-08-16 20:51:24 +0300

    If Eslse Username

---
+++ Examples/Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter username");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "sergey") 
+{
+    Console.WriteLine("Ura, eto je SERGEY");
+}
+else 
+{
+    Console.WriteLine("GO AWAY, ");
+    Console.WriteLine(username);
+}
