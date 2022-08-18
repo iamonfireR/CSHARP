8802a01efedbb038be6d19738e65f5204908ed90
unknown <iamonfire@yandex.ru>
2022-08-18 22:49:25 +0300

    2

---
+++ Examples/Homework/Homework001.2/Program.cs
@@ -0,0 +1,45 @@
+﻿int[] numbers = {2, 3, 7};
+int result = numbers[0];
+
+for (int i = 0; i < numbers.Length; i++) 
+{
+    if (result < numbers[i]) 
+    {
+        result = numbers[i];
+    }
+    i++;
+}
+
+Console.WriteLine(result);
+
+// ЗАДАЧА 2 //
+
+int[] numbers1 = {44, 5, 78};
+int result1 = numbers1[0];
+
+for (int i = 0; i < numbers.Length; i++) 
+{
+    if (result < numbers[i]) 
+    {
+        result = numbers[i];
+    }
+    i++;
+}
+
+Console.WriteLine(result1);
+
+// ЗАДАЧА 3 //
+
+int[] numbers2 = {22, 3, 9};
+int result2 = numbers2[0];
+
+for (int i = 0; i < numbers.Length; i++) 
+{
+    if (result < numbers[i]) 
+    {
+        result = numbers[i];
+    }
+    i++;
+}
+
+Console.WriteLine(result2);
