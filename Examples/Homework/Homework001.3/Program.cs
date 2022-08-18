4cad7bfdae2c1b993e6b11baba093d8c7b0ee261
unknown <iamonfire@yandex.ru>
2022-08-18 23:11:57 +0300

    3

---
+++ Examples/Homework/Homework001.3/Program.cs
@@ -0,0 +1,38 @@
+﻿int number = 4;                           // Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0 ) 
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
+
+// ЗАДАЧА 2
+
+
+int number1 = -3;                         
+
+if (number1 % 2 == 0 ) 
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
+
+// ЗАДАЧА 3
+
+
+int number2 = 7;                        
+
+if (number2 % 2 == 0 ) 
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
