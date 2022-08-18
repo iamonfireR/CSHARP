018e3434089436235dfda59baf1a1be6bbedaa59
unknown <iamonfire@yandex.ru>
2022-08-18 20:54:32 +0300

    Первая задача

---
+++ Examples/Homework/Homework001/Program.cs
@@ -0,0 +1,35 @@
+﻿int a = 5; // Convert.ToInt32(Console.ReadLine()); Ввод числа в консоль
+int b = 7; //  Convert.ToInt32(Console.ReadLine()); 
+
+if (a > b) 
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
+
+int a = 2; // Convert.ToInt32(Console.ReadLine());
+int b = 10; //  Convert.ToInt32(Console.ReadLine());
+
+if (a > b) 
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
+
+int a = -9; // Convert.ToInt32(Console.ReadLine());
+int b = -3; //  Convert.ToInt32(Console.ReadLine());
+
+if (a > b) 
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
