87ba350664e4b9df8ea092224c72cd8d78402ce3
unknown <iamonfire@yandex.ru>
2022-08-19 02:44:42 +0300

    tutor's changes

--- Examples/Homework/Homework001/Program.cs
+++ Examples/Homework/Homework001/Program.cs
@@ -1,5 +1,5 @@
-﻿int a = 5; // Convert.ToInt32(Console.ReadLine()); Ввод числа в консоль
-int b = 7; //  Convert.ToInt32(Console.ReadLine()); 
+﻿int a =Convert.ToInt32(Console.ReadLine());
+int b =Convert.ToInt32(Console.ReadLine()); 
 
 if (a > b) 
 {
@@ -8,32 +8,4 @@ if (a > b)
 else
 {
     Console.WriteLine(b);
-}
-
-// ЗАДАЧА 2 //
-
-int a1 = 2; // Convert.ToInt32(Console.ReadLine());
-int b1 = 10; //  Convert.ToInt32(Console.ReadLine());
-
-if (a1 > b1) 
-{
-    Console.WriteLine(a1);
-}
-else
-{
-    Console.WriteLine(b1);
-}
-
-// ЗАДАЧА 3 //
-
-int a2 = -9; // Convert.ToInt32(Console.ReadLine());
-int b2 = -3; //  Convert.ToInt32(Console.ReadLine());
-
-if (a2 > b2) 
-{
-    Console.WriteLine(a2);
-}
-else
-{
-    Console.WriteLine(b2);
 }
