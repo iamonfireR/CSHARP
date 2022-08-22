7775bc21aed088de6c98bab3dc25ce3441175784
unknown <iamonfire@yandex.ru>
2022-08-22 23:10:00 +0300

    First Task

---
+++ Examples/Homework/Homework2/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+
+int GenerateNumber(int generatedNum = 0)   // Функция генерирующая трехзначное число
+{
+    generatedNum = new Random().Next(100, 999);
+    Console.WriteLine("Генерирую трехзначное число...");
+    Console.WriteLine(generatedNum);
+    return generatedNum;
+}
+
+int SecondDigitOfGenNum(int secondDig) // Функция находящая вторую цифру числа.
+{
+    Console.WriteLine("Ищем вторую цифру сгенерированного трехзначного числа, она равна...");
+    secondDig = (secondDig / 10) % 10;
+    Console.WriteLine(secondDig);
+    return secondDig;
+   
+}
+
+int number = GenerateNumber();
+int result = SecondDigitOfGenNum(number);
+
+
+
