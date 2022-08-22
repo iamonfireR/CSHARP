1c5ae514c0964b39004770bfff4443533eae7ce3
unknown <iamonfire@yandex.ru>
2022-08-23 02:57:10 +0300

    Task 3

---
+++ Examples/Homework/Homework2/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿int dayOfTheWeekNum = Convert.ToInt32(Console.ReadLine());
+ 
+switch (dayOfTheWeekNum)
+{
+    case 1:
+        Console.WriteLine("Понедельник- НЕТ");
+        break;
+    case 2:
+        Console.WriteLine("Вторник- НЕТ");
+        break;
+    case 3:
+        Console.WriteLine("Среда- НЕТ");
+        break;
+    case 4:
+        Console.WriteLine("Четверг- НЕТ");
+        break;
+    case 5:
+        Console.WriteLine("Пятница- НЕТ");
+        break;
+    case 6:
+        Console.WriteLine("Суббота- ДА");
+        break;
+    case 7:
+        Console.WriteLine("Воскресенье- ДА");
+        break;      
+    default:
+        Console.WriteLine("Некорректный номер дня недели");
+        break;       
+}
