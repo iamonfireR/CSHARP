8f5929774341f652ec1c043eba403e491ff6081d
unknown <iamonfire@yandex.ru>
2022-08-23 05:06:11 +0300

    исправление

--- Examples/Homework/Homework2/Task4/Program.cs
+++ Examples/Homework/Homework2/Task4/Program.cs
@@ -4,7 +4,6 @@
 // на вход число и проверяет, кратно ли оно одновременно 7 и 23. 14 -> нет 46 -> нет 161 -> да
 
 int number = Convert.ToInt32(Console.ReadLine());
-int result = 0;
 
 if (number % 7 == 0 & number % 23 == 0) 
 {
