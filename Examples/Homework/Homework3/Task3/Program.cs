2691731d0535fa1c73d3562f69cc16245ea54e31
unknown <iamonfire@yandex.ru>
2022-08-27 05:13:05 +0300

    task 3

---
+++ Examples/Homework/Homework3/Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int numberN = int.Parse(Console.ReadLine());
+        double result;
+
+        for (int i = 1; i <= numberN; i++)
+        {
+            result = Math.Pow(i, 3);
+            Console.WriteLine(result);
+        }
+    } 
+}
