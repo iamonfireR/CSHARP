4c276387eff804420bb2dae71f09a54a772ee47c
iamonfireR <89128191+iamonfireR@users.noreply.github.com>
2022-09-02 11:48:49 +0300

    task4

---
+++ Examples/Homework/Homework5/Task4/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+
+class Program
+{   public static void Main(string[] args)
+    {
+        int[] ArrayFill(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new Random().Next(0, 100);
+            }
+
+            Console.WriteLine(string.Join(",", arr));
+            return arr;
+        }
+
+        int[] MinMaxFinder(int[] arr)
+        {
+            int minN = arr[1];
+            int maxN = arr[1];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int temp = 0;
+                if (arr[i] > maxN) maxN = arr[i];
+                if (arr[i] < minN) minN = arr[i]; 
+            }
+
+            int result = maxN - minN;
+
+            Console.WriteLine(result);
+
+            return arr;
+        }
+
+        int size = new Random().Next(10, 50);
+        int[] numberArr = new int[size];
+
+        ArrayFill(numberArr);
+        MinMaxFinder(numberArr);
+    }
+}
